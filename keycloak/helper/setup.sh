a2d3e57b8957e69216e5e734c100ec9dd666b9ee
yreinhar <m.y.reinhar@gmail.com>
2026-04-14 11:54:39 +0200

    feat: add manifests

---
+++ keycloak/helper/setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+KC_POD=$(kubectl get pod -o name | grep keycloak | head -n1 | cut -d/ -f2)
+
+kcadm() {
+  kubectl exec "$KC_POD" -- /opt/keycloak/bin/kcadm.sh "$@"
+}
+
+echo "Login to Keycloak..." >&2
+kcadm config credentials --server http://localhost:8080 --realm master --user admin --password admin
+
+echo "Create realm..." >&2
+kcadm create realms \
+    -s realm="$KEYCLOAK_REALM" \
+    -s enabled=true
+
+echo "Create identity provider..." >&2
+kcadm create identity-provider/instances \
+    -r "$KEYCLOAK_REALM" \
+    -s alias=kubernetes \
+    -s providerId="$KEYCLOAK_PROVIDER" \
+    -s config='{"issuer": "https://kubernetes.default.svc.cluster.local"}'
+
+echo "Create client..." >&2
+kcadm create clients \
+    -r "$KEYCLOAK_REALM" \
+    -s clientId="$KEYCLOAK_CLIENT" \
+    -s serviceAccountsEnabled=true \
+    -s clientAuthenticatorType=federated-jwt \
+    -s attributes="{ \"jwt.credential.issuer\": \"kubernetes\", \"jwt.credential.sub\": \"system:serviceaccount:$APP_NAMESPACE:my-serviceaccount\" }"
